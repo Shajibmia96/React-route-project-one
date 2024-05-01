b389ec9459c6c2ab7554bd4b2c42c91a67c5f8b2
Shajib96 <shajibahamedornob@gmail.com>
2024-05-02 00:04:16 +0600

    added some prodicts by click of button and delete all data by delete button

--- src/Pages/Favorite/Favorite.jsx
+++ src/Pages/Favorite/Favorite.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+import FavoriteCard from "../../Components/FavoriteCard/FavoriteCard";
+
 
 const Favorite = () => {
+
+    const [favorite , setFavorite] = useState([])
+    const [notFount , setNotFount] = useState(false)
+
+    useEffect(()=>{
+            const favoritesItems = JSON.parse(localStorage.getItem('favorites'))
+            if(favoritesItems){
+                 setFavorite(favoritesItems)
+            }else{
+                setNotFount('No Data Fount')
+            }
+    } , []);
+
+     const handlesRemove = () =>{
+          localStorage.clear()
+          setFavorite([])
+          setNotFount('No Data Fount')
+     }
     return (
         <div>
-            <h1>Favorite</h1>
+           {
+            notFount ? <h1 className="text-2xl flex justify-center items-center h-screen">{notFount}</h1> : <div>
+
+                {
+                    favorite.length > 0 && <button 
+                     onClick={handlesRemove}
+                    className="px-4 bg-orange-100 block mx-auto">Delete all favorite </button>
+                }
+                  
+                <div className=" py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-10">
+                     {
+                        favorite.map(phone => <FavoriteCard key={phone.id} phone={phone}></FavoriteCard> )
+                     }
+                </div>
+            </div>
+           }
         </div>
     );
 };
